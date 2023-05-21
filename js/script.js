37c2e91d735463a392dea3b039dd60d3a7b79937
Niloufar <116565518+Niloufar28@users.noreply.github.com>
2023-05-21 14:56:41 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const contextMenu = document.getElementById("contextMenu")
+
+function contextHandler (event) {
+    event.preventDefault()
+    if (contextMenu.style.display === "none") {
+        contextMenu.style.left = event.pageX + "px"
+        contextMenu.style.top = event.pageY + "px"
+        contextMenu.style.display = "block"
+    } else {
+        contextMenu.style.left = event.pageX + "px"
+        contextMenu.style.top = event.pageY + "px"
+    }
+}
+
+function clickHandler () {
+    contextMenu.style.display = "none"
+}
+
+function keyDownHandler (event) {
+    if (event.keyCode === 27) {
+        contextMenu.style.display = "none"
+    }
+}
+
+document.body.addEventListener("contextmenu", contextHandler)
+document.body.addEventListener("click", clickHandler)
+document.body.addEventListener("keydown", keyDownHandler)
